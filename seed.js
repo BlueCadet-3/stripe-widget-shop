f24e8a53473ae2252bf071a6eff0a1e88051c089
Richard Williams <richelliswilliams@gmail.com>
2021-03-12 12:12:26 -0800

    add seed to database

---
+++ seed.js
@@ -0,0 +1,96 @@
+require('dotenv').config();
+require('./config/database');
+
+const Widget = require('./models/widget');
+const Category = require('./models/category');
+// const category = require('./models/category');
+
+(async function() {
+
+    await Category.deleteMany({});
+    const categories = await Category.create([
+        {
+            name: 'Blue',
+        }, 
+        {
+            name: 'Red',
+        },
+        {
+            name: 'Green'
+        }
+    ]);
+
+
+
+    await Widget.deleteMany({});
+    const widgets = await Widget.create([
+        {
+            name: "Small Blue Widget",
+            price: 10,
+            imageUrl: 'noUrlYet',
+            description: 'A fancy small blue widget',
+            category: categories[0],
+        },
+        {
+            name: "Medium Blue Widget",
+            price: 20,
+            imageUrl: 'noUrlYet',
+            description: 'A fancy small blue widget',
+            category: categories[0],
+        },
+        {
+            name: "Large Blue Widget",
+            price: 30,
+            imageUrl: 'noUrlYet',
+            description: 'A fancy small blue widget',
+            category: categories[0],
+        },
+        {
+            name: "Small Red Widget",
+            price: 10,
+            imageUrl: 'noUrlYet',
+            description: 'A fancy small red widget',
+            category: categories[1],
+        },
+        {
+            name: "Medium Red Widget",
+            price: 20,
+            imageUrl: 'noUrlYet',
+            description: 'A fancy medium red widget',
+            category: categories[1],
+        },
+        {
+            name: "Large Red Widget",
+            price: 30,
+            imageUrl: 'noUrlYet',
+            description: 'A fancy large red widget',
+            category: categories[1],
+        },
+        {
+            name: "Small Green Widget",
+            price: 10,
+            imageUrl: 'noUrlYet',
+            description: 'A fancy small green widget',
+            category: categories[2],
+        },
+        {
+            name: "Medium Green Widget",
+            price: 20,
+            imageUrl: 'noUrlYet',
+            description: 'A fancy medium green widget',
+            category: categories[2],
+        },
+        {
+            name: "Large Green Widget",
+            price: 30,
+            imageUrl: 'noUrlYet',
+            description: 'A fancy large green widget',
+            category: categories[2],
+        },
+    ]);
+
+    console.log(widgets)
+
+    process.exit();
+
+})();
